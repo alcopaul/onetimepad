2c66801eca54cf7c9c1aa57e68634d7b8701f0b8
Paul Glenerson B. Amurao <paul.amurao@gmail.com>
2016-04-04 14:56:57 -0400

    Create encrypt.cpp

---
+++ encrypt.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string lib64 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string str1 = "INDIGOREDMUSALTP2T";
+    string key1 = "PAULYELENA4028PLOT";
+	string result = "";
+	for (int i = 0; i < str1.length(); i++)
+	{
+		int k = lib64.find(str1.substr(i, 1)) + lib64.find(key1.substr(i,1));
+		if (k >= 36)
+		{
+			k = k - 36;
+		}
+		result += lib64.substr(k, 1);
+	}
+	cout << result << '\n';
+	char c = getchar();
+	return 0;
+}
